ea269b96fdf4d9edac1e68403bc3a03b325b4dc7
KAIF ANSARI <64328313+lovepains@users.noreply.github.com>
2020-04-27 15:15:44 +0530

    Update short.sh

--- short.sh
+++ short.sh
@@ -1,28 +1,6 @@
 #Tool By LOVE PAINS 
 #ThAnKS For Supporting
 
-apt-get update -y
-echo " "
-apt-get upgrade -y
-echo " "
-pkg install figlet -y
-echo " "
-pkg install toilet -y
-echo " "
-pkg install cowsay -y
-echo " "
-pkg install nano -y
-echo " "
-pkg install ruby -y
-echo " "
-gem install lolcat
-echo " "
-pkg install nano -y
-echo ""
-pkg install mpv -y
-echo " "
-pkg istall cat -y
-clear
 
 ################                                                       cyn='\e[0;36m'
 
